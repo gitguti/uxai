7b4267fc442e5838dbba87f2e50059e449d82f6e
gitguti <gitmelgtrrz1@gmail.com>
2023-07-20 15:47:47 -0400

    updated content

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,7 @@ function App() {
     <div className='hero-text'>
     <h1 className='text-focus-in'>Empower Your Time with AI Automation</h1>
     <div className='text-pop-up-top'>
-    <p>Create compelling UX/UI case studies effortlessly and focus on what truly matters.</p>
-    <p>Sign up and be the first to be notified when we open our doors</p>
+    <p>Create compelling UX/UI case studies effortlessly and focus on what truly matters. Sign up and be the first to be notified when we open our doors</p>
     <PopupButton id="O5GxuX60" style={{ width: '300px' }} className="my-form">
     Suscribe </PopupButton>
     </div>
