1793516d97165055f82d88abb8ddfe4c078e1d54
gitguti <gitmelgtrrz1@gmail.com>
2023-07-20 15:55:07 -0400

    updated content again

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ function App() {
     <Spline scene="https://prod.spline.design/Kzxb2QVVKWxB3ked/scene.splinecode" className='spline' />
     <div className='hero-relative'>
     <div className='hero-text'>
-    <h1 className='text-focus-in'>Empower Your Time with AI Automation</h1>
+    <h1 className='text-focus-in'>Be the First to Experience AI-Powered Case Studies</h1>
     <div className='text-pop-up-top'>
-    <p>Create compelling UX/UI case studies effortlessly and focus on what truly matters. Sign up and be the first to be notified when we open our doors</p>
+    <p>Simplify your UX/UI case study creation effortlessly with AI automation. Join our waiting list and be among the first to be notified when we launch</p>
     <PopupButton id="O5GxuX60" style={{ width: '300px' }} className="my-form">
-    Suscribe </PopupButton>
+    Join Waiting List </PopupButton>
     </div>
     </div>
     </div>
