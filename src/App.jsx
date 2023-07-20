66667e445030ef02fe153be49477822da14638f2
gitguti <gitmelgtrrz1@gmail.com>
2023-07-19 23:15:31 -0400

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,15 @@ function App() {
     <>
     <div className='hero'>
     <Spline scene="https://prod.spline.design/Kzxb2QVVKWxB3ked/scene.splinecode" className='spline' />
-    <div style={{zIndex: 100, position: 'relative', top: '30%', width: '85%'}}>
+    <div className='hero-relative'>
+    <div className='hero-text'>
     <h1 className='text-focus-in'>Empower Your Time with AI Automation</h1>
     <div className='text-pop-up-top'>
-    <p style={{width: '70%'}}>Streamline your workflow and elevate your productivity with the transformative power of AI automation. Create compelling UX/UI case studies effortlessly and focus on what truly matters.</p>
-    <p style={{width: '70%'}}>Sign up and be the first to be notified when we open our doors</p>
-    <PopupButton id="O5GxuX60" style={{ width: '30%' }} className="my-form">
+    <p>Streamline your workflow and elevate your productivity with the transformative power of AI automation. Create compelling UX/UI case studies effortlessly and focus on what truly matters.</p>
+    <p>Sign up and be the first to be notified when we open our doors</p>
+    <PopupButton id="O5GxuX60" style={{ width: '300px' }} className="my-form">
     Suscribe </PopupButton>
+    </div>
     </div>
     </div>
     </div>
